b923a8143360ea4d7131acbf4c1c987767ef91e9
Emilian Dragan <emilian.dragan@infoworld.ro>
2016-12-05 17:19:28 +0200

    added 2 more functions to compare and new sections: procedure and immunization plus minor changes to allergyIntolerace, condition, medication administration, medicationprescription

--- example/compare.js
+++ example/compare.js
@@ -1,9 +1,24 @@
 var match = require("../index.js");
+var bunyan = require("bunyan");
+
+var log = bunyan.createLogger({
+    name: "log",
+    streams: [{
+        level: "debug",
+        path: "./log.log",
+        period: "1d",
+        count: "7"
+    }, {
+        level: "error",
+        stream: process.stdout
+    }
+    ]
+});
 
 // files to compare
 var binary = require("./binary2.json");
 var everything = require("./everything2.json");
 
-var result = match.match(binary, everything);
+var result = match.match(binary, everything, log);
 
 console.log(JSON.stringify(result, null, 4));
